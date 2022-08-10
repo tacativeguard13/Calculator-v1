34580ff263d38b332044565a811e7e2f6405275f
tacativeguard13 <87353368+tacativeguard13@users.noreply.github.com>
2022-08-10 18:02:57 +0530

    Commit 3 (Bugs Sloved)

--- Interface.py
+++ Interface.py
@@ -10,9 +10,7 @@ print('''1: Addition
 4: Division
 5: Find the square
 6: Find the cube
-7: Find the Square Root
-8: Find the Cube Root
-9: Find percentage''')
+7: Find percentage''')
 work = input("What work do you want to do ?\n")
 
 if work == '1':
@@ -28,10 +26,6 @@ elif work == '5':
 elif work == '6':
     Calculations.Cube()
 elif work == '7':
-    Calculations.SquareRoot()
-elif work == '8':
-    Calculations.CubeRoot()
-elif work == '9':
     Calculations.Percentage()
 else:
     print("Plese define a Work to do")
